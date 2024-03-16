7ed07277fee8f276c19dcaa4bc4101b09caa5918
Ukudot <giovannipanico314@gmail.com>
2024-03-16 22:27:20 +0100

    code commented

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -1,14 +1,18 @@
+section .note.GNU-stack
 section .text
 global ft_strcpy
 
 ft_strcpy:
-	mov rax, rdi 
+	mov rax, rdi ; save the address of destination first char
 
-ft_strcpy_loop:
-	mov r10b, [rsi]
-	mov [rdi], r10b
-	inc rsi
-	inc rdi
-	cmp byte [rsi], 0
-	jne ft_strcpy_loop
-	ret
+_ft_strcpy_loop:
+	movzx r10, byte [rsi] ; move the char pointed by source in r10 with zero padding
+	mov [rdi], r10b ; move the char stored in r10b in the destination
+	cmp byte [rsi], 0 ; check if the byte pointed by source is null
+	jne _increment ; if the char is not null increment the two pointer
+	ret ; return the first char address of destination string stored in rax
+
+_increment:
+	inc rsi ; cycle the source string
+	inc rdi ; increment the pointer of destination string
+	jmp _ft_strcpy_loop ; loop back in order to copy another char
--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -1,15 +1,16 @@
+section .note.GNU-stack
 section .text
 global ft_strlen
 
 ft_strlen:
-	mov rax, 0
+	xor rax, rax ; initialize to 0 the return register
 
 _strlen_loop:
-	cmp byte [rdi], 0
-	je _end
-	inc rdi
-	inc rax
-	jmp _strlen_loop
+	cmp byte [rdi], 0 ; check if the char is a null character
+	je _end ; if the char is null jump to end
+	inc rdi ; cycle the string
+	inc rax ; increment counter
+	jmp _strlen_loop ; loop back to check if the char is null
 
 _end:
-	ret
+	ret ; return the value stored in rax
